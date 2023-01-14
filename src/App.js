b0eb822975ea633ecd094f3d56cd1ac8c060e220
MarcosKern <vkg.marcos@gmail.com>
2023-01-14 15:53:13 -0300

    cm3 - timer runs in title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import SetTimeInterval from "./components/SetTimeInterval";
 import StartButton from "./components/StartButton";
 import Timer from "./components/Timer";
+import MyContext from "./context/Context";
 import './styles/screen.css'
 
 function App() {
+  const { totalHours, totalMinutes, totalSeconds } = useContext(MyContext)
   return (
     <section className="mainScreen">
+      <head>
+        <title>{ `${totalHours}:${totalMinutes}:${totalSeconds}` }</title>
+      </head>
       <Timer />
       <SetTimeInterval />
       <StartButton />
