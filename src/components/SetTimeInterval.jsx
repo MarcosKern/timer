e37db63b03a0d9b62138a5fc32de842944ddaaf3
MarcosKern <vkg.marcos@gmail.com>
2023-01-16 13:59:57 -0300

    cm9 - fix timer values

--- src/components/SetTimeInterval.jsx
+++ src/components/SetTimeInterval.jsx
@@ -9,30 +9,46 @@ export default function SetTimeInterval() {
     timerAppears
   } = useContext(MyContext);
 
-  const handleChange = (eve, func) => {
+  const handleChange = (eve, func, newCase) => {
     const {
       target: {
         value
       }
     } = eve;
-    const newValue = value
-    func(newValue.padStart(2, '0'))
+    const newValue = Number(value);
+    if (Number.isInteger(newValue)) {
+      console.log('oi')
+      switch (newCase) {
+        case 'hour':
+          if (newValue <= 24 && newValue >= 0) {
+            return func(String(newValue).padStart(2, '0'))
+          } else break;
+        case 'minutes':
+          if (newValue <= 59 && newValue >= 0) {
+            return func(String(newValue).padStart(2, '0'))
+          } else break;
+        case 'seconds':
+          if (newValue <= 60 && newValue >= 0) {
+            return func(String(newValue).padStart(2, '0'))
+          } else break;
+        default: break;
+      }
+    }
   }
 
   return (
     <section className={`setTime ${ !timerAppears && 'hidden' }`}>
       <input type="number" name="Hours" value={ totalHours }
-        id="h" max='24' min='00'
-        contentEditable='true'
-        onChange={(event) => handleChange(event, setTotalHours)}
+        id="h" max='24' min='00' readOnly={ !timerAppears }
+        onChange={(event) => handleChange(event, setTotalHours, 'hour')}
       />
-      <input type="number" name="Minutes"
+      <input type="number" name="Minutes" readOnly={ !timerAppears }
         id="m" max='59' min='00' value={ totalMinutes }
-        onChange={(event) => handleChange(event, setTotalMinutes)}
+        onChange={(event) => handleChange(event, setTotalMinutes, 'minutes')}
       />
-      <input type="number" name="Seconds"
+      <input type="number" name="Seconds" readOnly={ !timerAppears }
         id="s" max='60' min='00' step='10' value={ totalSeconds }
-        onChange={(event) => handleChange(event, setTotalSeconds)}
+        onChange={(event) => handleChange(event, setTotalSeconds,'seconds')}
       />
     </section>
   )
