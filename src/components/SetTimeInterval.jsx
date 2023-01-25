2ff4d1e5de25bdc870cf5837f3e00da43124a2e3
MarcosKern <vkg.marcos@gmail.com>
2023-01-25 14:34:58 -0300

    cm11 - Increment/decrement buttons att

--- src/components/SetTimeInterval.jsx
+++ src/components/SetTimeInterval.jsx
@@ -3,55 +3,48 @@ import MyContext from '../context/Context'
 import '../styles/setTime.css'
 
 export default function SetTimeInterval() {
-  const { setTotalHours, totalHours,
-    setTotalMinutes, totalMinutes,
-    setTotalSeconds, totalSeconds,
-    timerAppears
-  } = useContext(MyContext);
+  const {
+    setTotalHours,
+    totalHours,
+    setTotalMinutes,
+    totalMinutes,
+    setTotalSeconds,
+    totalSeconds,
+    timerAppears,
+  } = useContext(MyContext)
 
-  const handleChange = (eve, func, newCase) => {
-    const {
-      target: {
-        value
-      }
-    } = eve;
-    const newValue = Number(value);
-    if (Number.isInteger(newValue)) {
-      console.log('oi')
-      switch (newCase) {
-        case 'hour':
-          if (newValue <= 24 && newValue >= 0) {
-            return func(String(newValue).padStart(2, '0'))
-          } else break;
-        case 'minutes':
-          if (newValue <= 59 && newValue >= 0) {
-            return func(String(newValue).padStart(2, '0'))
-          } else break;
-        case 'seconds':
-          if (newValue <= 60 && newValue >= 0) {
-            return func(String(newValue).padStart(2, '0'))
-          } else break;
-        default: break;
-      }
+  const decrement = (func, value, quantity) => {
+    if (value > 0) {
+      const updatedValue = Number(value) - quantity
+      func(updatedValue.toString().padStart(2, '0'))
     }
+    return
+  }
+
+  const increment = (func, value, quantity, limit) => {
+    if (value < limit) {
+      const updatedValue = Number(value) + quantity
+      func(updatedValue.toString().padStart(2, '0'))
+    }
+    return
   }
 
   return (
-    <section className={`setTime ${ !timerAppears && 'hidden' }`}>
+    <section className={`setTime ${!timerAppears && 'hidden'}`}>
       <div>
-        <button>-</button>
-        <p>{ totalHours }</p>
-        <button>+</button>
+        <button className='decrement' onClick={ () => decrement(setTotalHours, totalHours, 1) }>-</button>
+        <p>{totalHours}</p>
+        <button className='increment' onClick={ () => increment(setTotalHours, totalHours, 1, 24) }>+</button>
       </div>
       <div>
-        <button>-</button>
-        <p>{ totalMinutes }</p>
-        <button>+</button>
+        <button className='decrement' onClick={ () => decrement(setTotalMinutes, totalMinutes, 1) }>-</button>
+        <p>{totalMinutes}</p>
+        <button className='increment' onClick={ () => increment(setTotalMinutes, totalMinutes, 1, 59) }>+</button>
       </div>
       <div>
-        <button>-</button>
-        <p>{ totalSeconds }</p>
-        <button>+</button>
+        <button className='decrement' onClick={ () => decrement(setTotalSeconds, totalSeconds, 10) }>-</button>
+        <p>{totalSeconds}</p>
+        <button className='increment' onClick={ () => increment(setTotalSeconds, totalSeconds, 10, 60) }>+</button>
       </div>
     </section>
   )
